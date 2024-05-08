db4b38083313e0d1d9703927433256faabe9452c
RoseReyes <roseannlorinreyes@gmail.com>
2024-05-07 20:54:14 -0700

    display popular movies

--- js/script.js
+++ js/script.js
@@ -2,6 +2,53 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+const displayPopularMovies = async () => {
+  const {movies} = await fetchAPIData('movie/popular');
+
+  movies.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+
+    div.innerHTML = `<a href='movie-details.html?id=${movie.id}'>
+          ${
+            movie.poster_path
+              ? `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class='card-img-top'
+            alt="${movie.title}"
+          />`
+              : `<img
+            src="../images/no-image.jpg"
+            class='card-img-top'
+            alt='Movie Title'
+          />`
+          }
+        </a>
+        <div class='card-body'>
+          <h5 class='card-title'>${movie.title}</h5>
+          <p class='card-text'>
+            <small class='text-muted'>Release: ${movie.release_date}</small>
+          </p>
+        </div>`;
+
+    // append to main div
+    document.getElementById('popular-movies').appendChild(div);
+  });
+};
+
+// Fetch data from API
+const fetchAPIData = async (endpoint) => {
+  const API_KEY = '42800da2d7c030192c267c15664aab8f';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+  const data = await response.json();
+
+  return data;
+};
+
 // Highlight active link
 const highlightActiveLink = () => {
   const links = document.querySelectorAll('.nav-link');
@@ -17,7 +64,7 @@ const init = () => {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('Shows');
