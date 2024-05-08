b5c5a6c7a0c89b3301858a87b631918b7e034ec1
RoseReyes <roseannlorinreyes@gmail.com>
2024-05-07 21:18:25 -0700

    spinner and tv shows

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ const global = {
 };
 
 const displayPopularMovies = async () => {
-  const {movies} = await fetchAPIData('movie/popular');
+  const {results} = await fetchAPIData('movie/popular');
 
-  movies.forEach((movie) => {
+  results.forEach((movie) => {
     const div = document.createElement('div');
     div.classList.add('card');
 
@@ -36,20 +36,67 @@ const displayPopularMovies = async () => {
   });
 };
 
+const displayPopularTVShows = async () => {
+  const {results} = await fetchAPIData('tv/popular');
+  console.log(results);
+
+  results.forEach((tv) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+
+    div.innerHTML = `<a href="tv-details.html?id=${tv.id}">
+            ${
+              tv.poster_path
+                ? `<img
+            src="https://image.tmdb.org/t/p/w500${tv.poster_path}"
+            class='card-img-top'
+            alt="${tv.name}"
+          />`
+                : `<img
+            src="../images/no-image.jpg"
+            class='card-img-top'
+            alt='Movie Title'
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${tv.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Aired: ${tv.first_air_date}</small>
+            </p>
+          </div>`;
+
+    document.getElementById('popular-shows').appendChild(div);
+  });
+};
+
 // Fetch data from API
 const fetchAPIData = async (endpoint) => {
   const API_KEY = '42800da2d7c030192c267c15664aab8f';
   const API_URL = 'https://api.themoviedb.org/3/';
+
+  toggleSpinner();
 
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
   const data = await response.json();
 
+  toggleSpinner();
+
   return data;
 };
 
-// Highlight active link
+const toggleSpinner = () => {
+  const el = document.querySelector('.spinner');
+
+  if (!el.classList.contains('show')) {
+    el.classList.add('show');
+  } else {
+    el.classList.remove('show');
+  }
+};
+
 const highlightActiveLink = () => {
   const links = document.querySelectorAll('.nav-link');
   links.forEach((link) => {
@@ -67,7 +114,7 @@ const init = () => {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularTVShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
